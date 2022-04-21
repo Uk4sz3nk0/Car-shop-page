0c71fa7d33c482d4a60bdad7c7cd31f9c41904a7
Uk4sz3nk0 <gerdzio53@gmail.com>
2022-04-21 12:02:12 +0200

    Project Init

---
+++ Car-shop/src/main/java/pl/Uk4szenk0/Car/shop/CarShopApplication.java
@@ -0,0 +1,13 @@
+package pl.Uk4szenk0.Car.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShopApplication.class, args);
+	}
+
+}
---
+++ Car-shop/src/test/java/pl/Uk4szenk0/Car/shop/CarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Uk4szenk0.Car.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
